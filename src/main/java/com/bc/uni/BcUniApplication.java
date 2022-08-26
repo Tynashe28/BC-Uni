aec38aef677280cdfdeed997cbeba52cb524d82c
Tynashe28 <n01519975j@students.nust.ac.zw>
2022-08-26 16:51:16 +0200

    Project Init

---
+++ src/main/java/com/bc/uni/BcUniApplication.java
@@ -0,0 +1,13 @@
+package com.bc.uni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcUniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcUniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bc/uni/BcUniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bc.uni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcUniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
